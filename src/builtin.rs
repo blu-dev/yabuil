f65b35c66c145a9425ca3a487140ea0ab0af4874
blu-dev <the.blu.dev@gmail.com>
2023-12-12 16:24:07 -0700

    fix: Fix color interpolation

--- src/builtin.rs
+++ src/builtin.rs
@@ -16,6 +16,20 @@ fn deserialize_color<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Color
 #[derive(Deserialize, Serialize, Reflect)]
 pub struct ColorAnimation(#[serde(deserialize_with = "deserialize_color")] Color);
 
+fn convert_color(color: Color) -> colorgrad::Color {
+    colorgrad::Color::new(
+        color.r() as f64,
+        color.g() as f64,
+        color.b() as f64,
+        color.a() as f64,
+    )
+}
+
+fn linear_and_bright(color: Color) -> (Vec4, f32) {
+    let [r, g, b, a] = color.as_linear_rgba_f32();
+    (Vec4::new(r, g, b, a), (r + g + b + a).powf(0.43))
+}
+
 impl LayoutAnimationTarget for ColorAnimation {
     const NAME: &'static str = "Color";
 
@@ -28,9 +42,16 @@ impl LayoutAnimationTarget for ColorAnimation {
     ) {
         let color = match previous {
             Some(Self(prev_color)) => {
-                let a = (1.0 - progress) * prev_color.a() + progress * self.0.a();
-                let new_hsl = prev_color.as_hsla() * (1.0 - progress) + self.0.as_hsla() * progress;
-                new_hsl.with_a(a)
+                let (linear_a, bright_a) = linear_and_bright(*prev_color);
+                let (linear_b, bright_b) = linear_and_bright(self.0);
+                let intensity =
+                    (bright_a * (1.0 - progress) + bright_b * progress).powf(0.43f32.recip());
+                let mut color = linear_a * (1.0 - progress) + linear_b * progress;
+                let sum = color.x + color.y + color.z + color.w;
+                if sum != 0.0 {
+                    color = color * intensity / sum;
+                }
+                Color::rgba_linear(color.x, color.y, color.z, color.w)
             }
             None => self.0,
         };
